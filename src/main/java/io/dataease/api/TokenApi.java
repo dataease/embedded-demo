212ae42d7e88ac6a651f4234dc53f23367f31e86
fit2cloud-chenyw <yawen.chen@fit2cloud.com>
2024-08-05 18:53:33 +0800

    feat(X-Pack): 嵌入式token增加传参功能

--- src/main/java/io/dataease/api/TokenApi.java
+++ src/main/java/io/dataease/api/TokenApi.java
@@ -3,11 +3,16 @@ package io.dataease.api;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTCreator;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.dataease.api.dto.SettingVO;
 import io.dataease.api.utils.SettingUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @RestController
@@ -20,11 +25,14 @@ public class TokenApi {
      * @return
      */
     @GetMapping("/token/{account}")
-    public String generate(@PathVariable("account") String account) {
+    public String generate(@PathVariable("account") String account) throws JsonProcessingException {
         SettingVO vo = SettingUtils.read();
         Algorithm algorithm = Algorithm.HMAC256(vo.getAppSecret());
         JWTCreator.Builder builder = JWT.create();
-        builder.withClaim("account", account).withClaim("appId", vo.getAppId());
+        List<String> ipList = new ArrayList<>();
+        ipList.add("192.168.1.10");
+        ipList.add("192.168.1.20");
+        builder.withClaim("account", account).withClaim("appId", vo.getAppId()).withClaim("arg1", "果粒果汁").withClaim("arg2", ipList);
         return builder.sign(algorithm);
     }
 }
